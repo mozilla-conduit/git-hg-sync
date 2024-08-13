f84e89133ad16aefef4074c12b21b0c8dbaf4137
Frank Bessou <frank.bessou@logilab.fr>
2024-08-13 08:24:42 +0200

    feat: allow regexes in configuration

--- git_hg_sync/mapping.py
+++ git_hg_sync/mapping.py
@@ -1,4 +1,6 @@
 from dataclasses import dataclass
+from functools import cached_property
+import re
 
 import pydantic
 
@@ -26,17 +28,28 @@ class Mapping(pydantic.BaseModel):
     source: MappingSource
     destination: MappingDestination
 
+    @cached_property
+    def _branch_pattern(self) -> re.Pattern:
+        return re.compile(self.source.branch_pattern)
+
     def match(self, event: Push) -> list[MappingMatch]:
         if event.repo_url != self.source.url:
             return []
         matches: list[MappingMatch] = []
         for branch_name, commit in event.branches.items():
-            if branch_name == self.source.branch_pattern:
-                matches.append(
-                    MappingMatch(
-                        source_commit=commit,
-                        destination_url=self.destination.url,
-                        destination_branch=self.destination.branch,
-                    )
+            if not self._branch_pattern.match(branch_name):
+                continue
+            destination_url = re.sub(
+                self._branch_pattern, self.destination.url, branch_name
+            )
+            destination_branch = re.sub(
+                self._branch_pattern, self.destination.branch, branch_name
+            )
+            matches.append(
+                MappingMatch(
+                    source_commit=commit,
+                    destination_url=destination_url,
+                    destination_branch=destination_branch,
                 )
+            )
         return matches
