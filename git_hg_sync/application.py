72aaf19c50fd6f99baf3c0fcf01ed944be5be56a
Olivier Giorgis <ogiorgis@logilab.fr>
2024-10-25 11:12:53 +0200

    fix: specify the event id in the log

--- git_hg_sync/application.py
+++ git_hg_sync/application.py
@@ -38,7 +38,7 @@ class Application:
         self._worker.run()
 
     def _handle_push_event(self, push_event: Push) -> None:
-        logger.info("Handling push event.")
+        logger.info(f"Handling push event: {push_event.pushid}")
         synchronizer = self._repo_synchronizers[push_event.repo_url]
         operations_by_destination: dict[str, list[SyncOperation]] = {}
 
