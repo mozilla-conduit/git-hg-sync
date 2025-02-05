4394271625072c15e8ee62cf3eeac48e6739d445
Olivier Mehani <omehani@mozilla.com>
2025-02-05 15:53:35 +1100

    config: allow to override pulse parameter via env

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 
+from pytest import MonkeyPatch
+
 from git_hg_sync.config import Config
 
 HERE = Path(__file__).parent
@@ -8,4 +10,28 @@ HERE = Path(__file__).parent
 def test_load_config() -> None:
     config = Config.from_file(HERE / "data" / "config.toml")
     assert not config.pulse.ssl
+    assert config.pulse.host == "pulse"
     assert config.branch_mappings[0].destination_branch == "default"
+
+
+def test_load_config_env_override(monkeypatch: MonkeyPatch) -> None:
+    pulse_env = {
+        "exchange": "overridden exchange",
+        "host": "overridden host",
+        "password": "overridden password",
+        "port": "overridden port",
+        "queue": "overridden queue",
+        "routing_key": "overridden routing_key",
+        "ssl": "false",
+        "userid": "overridden userid",
+    }
+
+    for key in pulse_env:  # noqa: PLC0206 We want the key only here...
+        monkeypatch.setenv(f"PULSE_{key}".upper(), pulse_env[key])
+
+    config = Config.from_file(HERE / "data" / "config.toml")
+
+    for key in pulse_env:  # noqa: PLC0206
+        assert getattr(config.pulse, key) == pulse_env[key], (
+            f"Pulse configuration not overridden for {key}"
+        )
