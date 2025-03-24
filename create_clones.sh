995aee0c47286506ef24da12d83ad04fc1a378ff
Olivier Mehani <omehani@mozilla.com>
2025-03-24 14:39:12 +1100

    fixup! docker: add create_clones.sh

--- create_clones.sh
+++ create_clones.sh
@@ -31,14 +31,12 @@ git config fetch.prune true
 # make git status and friends faster:
 # mv .git/hooks/fsmonitor-watchman.sample .git/hooks/query-watchman
 # git config core.fsmonitor .git/hooks/query-watchman
-# add git origin
-# git remote add git_chatzilla http://github.com/djangoliv/chatzilla.git
 
 # conf
 git config remote.origin.skipDefaultUpdate true
 git remote set-url --push origin hg::ssh://git@forge.extranet.logilab.fr/ogiorgis/chatzilla
 git config remote.origin.push +HEAD:refs/heads/branches/default/tip
-# git pull git_chatzilla
+
 # check
 git cinnabar fsck
 
