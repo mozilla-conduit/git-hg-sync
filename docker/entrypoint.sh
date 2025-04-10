2f6c2aab80dc488b2710d79ddacc19a1865ea10a
Olivier Mehani <omehani@mozilla.com>
2025-04-10 16:16:48 +1000

    docker: add support for SSH_USERNAME (bug 1959585)

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,12 +1,13 @@
-#!/bin/bash
+#!/bin/bash -e
 
 if [ -n "${SSH_PRIVATE_KEY:-}" ]; then
   echo "Importing SSH key supplied in \$SSH_PRIVATE_KEY ..."
   eval "$(ssh-agent -s)"
   ssh-add - <<< "${SSH_PRIVATE_KEY}"
-  SSH_PRIVATE_KEY='imported'
+  export SSH_PRIVATE_KEY='imported'
   export GIT_SSH_COMMAND="ssh -oStrictHostKeyChecking=accept-new"
 fi
+
 
 case "${1:-}" in
   "bash"|"sh")
