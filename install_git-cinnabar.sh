3074d26d3428c4101ff5699946f230302408cbe4
Connor Sheehan <sheehan@mozilla.com>
2025-05-04 20:27:23 -0400

    cinnabar: install latest changes from git-cinnabar upstream (Bug 1963805) (#43)
    Upgrade cinnabar to install the latest changes available in the
    upstream repo. This works around a bug in cinnabar tag pushing.

--- install_git-cinnabar.sh
+++ install_git-cinnabar.sh
@@ -2,6 +2,6 @@
 
 curl https://raw.githubusercontent.com/glandium/git-cinnabar/master/download.py -o download.py
 chmod u+x download.py
-./download.py --exact 0.7.2
+./download.py --exact 4beaf11666a48d5fac1660d6ceca09d1ffa406c6
 chmod a+x git-cinnabar
 chmod a+x git-remote-hg
