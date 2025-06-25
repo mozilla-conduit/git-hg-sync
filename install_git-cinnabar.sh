b7e203d16e29c02ef04f346c34daba96e24fa02f
Olivier Mehani <omehani@mozilla.com>
2025-06-25 16:23:31 +1000

    cinnabar: bump to current tip on main (bug 1973880)
    This is to include orphan tag branch support

--- install_git-cinnabar.sh
+++ install_git-cinnabar.sh
@@ -2,6 +2,6 @@
 
 curl https://raw.githubusercontent.com/glandium/git-cinnabar/master/download.py -o download.py
 chmod u+x download.py
-./download.py --exact 19ead4b40b57c96a02d8cc3927dd08bd7c55d1d9
+./download.py --exact 8a2959e3d04781179f80ba47288ee8af800112bb
 chmod a+x git-cinnabar
 chmod a+x git-remote-hg
