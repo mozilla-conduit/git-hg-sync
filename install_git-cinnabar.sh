abc045b50dcabfe43fa8b3f90da6458cb3e18119
Olivier Mehani <omehani@mozilla.com>
2025-07-16 17:43:25 +1000

    cinnabar: bump to 0.7.3 (bug 1964291)

--- install_git-cinnabar.sh
+++ install_git-cinnabar.sh
@@ -2,6 +2,6 @@
 
 curl https://raw.githubusercontent.com/glandium/git-cinnabar/master/download.py -o download.py
 chmod u+x download.py
-./download.py --exact 8a2959e3d04781179f80ba47288ee8af800112bb
+./download.py --exact 0.7.3
 chmod a+x git-cinnabar
 chmod a+x git-remote-hg
