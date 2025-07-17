e583a2b8f35b81d4d94b03a16a8176191c4a1b71
Olivier Mehani <shtrom-github@ssji.net>
2025-07-17 12:02:05 +1000

    cinnabar: bump to 0.7.3 (bug 1964291) (#65)
    * cinnabar: bump to 0.7.3 (bug 1964291)
    * cinnabar: use download.py script from target revision

--- install_git-cinnabar.sh
+++ install_git-cinnabar.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-curl https://raw.githubusercontent.com/glandium/git-cinnabar/master/download.py -o download.py
+VER=0.7.3
+
+curl https://raw.githubusercontent.com/glandium/git-cinnabar/${VER}/download.py -o download.py
 chmod u+x download.py
-./download.py --exact 8a2959e3d04781179f80ba47288ee8af800112bb
+./download.py --exact ${VER}
 chmod a+x git-cinnabar
 chmod a+x git-remote-hg
