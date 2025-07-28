08f08011bb47b404dd8f4cb748af983367ceb734
Olivier Mehani <omehani@mozilla.com>
2025-07-28 14:50:27 +1000

    cinnabar: bump to avoid duplicated tags (bug 1979342)

--- install_git-cinnabar.sh
+++ install_git-cinnabar.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-VER=0.7.3
+VER=3f0a8a4c35b5be88ddc229c0dd099b99c67483ce
 
 curl https://raw.githubusercontent.com/glandium/git-cinnabar/${VER}/download.py -o download.py
 chmod u+x download.py
