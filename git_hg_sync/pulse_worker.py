3fb90dbca5680be5a64aa6cda6d4124b2fbe5201
Olivier Mehani <omehani@mozilla.com>
2026-01-15 12:50:19 +1100

    fixup! pulse_worker: don't prefetch more than one message (bug 1981900)

--- git_hg_sync/pulse_worker.py
+++ git_hg_sync/pulse_worker.py
@@ -55,6 +55,9 @@ class PulseWorker(ConsumerMixin):
             self.task_queue,
             auto_declare=False,
             callbacks=[self.on_task],
+            # We only fetch one message at a time in case processing it fails.
+            # This allows us to ensure strict ordering, by re-receiving the same message
+            # on the next loop after having requeued it after failure.
             prefetch_count=1,
         )
         logger.debug(f"Using consumer {consumer=}")
