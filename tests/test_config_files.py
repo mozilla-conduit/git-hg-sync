4eefc99f091a5e710c4f1b5b692d3b0d35d99566
Olivier Mehani <omehani@mozilla.com>
2026-03-03 12:45:37 +1100

    tests: improve test_config_files to cover all test_config_files

--- tests/test_config_files.py
+++ tests/test_config_files.py
@@ -1,27 +1,26 @@
 from pathlib import Path
 
 import pytest
+from pydantic import ValidationError
 
 from git_hg_sync.config import Config
 
-HERE = Path(__file__).parent
+BASEDIR = Path(__file__).parent.parent
 
 
 @pytest.mark.parametrize(
     "config_file",
-    [
-        "config-development.toml",
-        "config-docker.toml",
-        "config-production.toml",
-        "config-staging.toml",
-    ],
+    list(BASEDIR.glob("config-*.toml"))
 )
-def test_config_files(config_file: str) -> None:
-    config = Config.from_file(HERE / ".." / config_file)
+def test_config_files(config_file: Path) -> None:
+    try:
+        config = Config.from_file(config_file)
+    except ValidationError as exc:
+        raise AssertionError(f"Syntax in {config_file}") from exc
 
     # We just do a shallow verification. What we really care is that the file could be
     # loaded correctly.
-    assert config.pulse
-    assert config.tracked_repositories
-    assert config.branch_mappings
-    assert config.tag_mappings
+    assert config.pulse, f"`pulse` section missing in {config_file}"
+    assert config.tracked_repositories, f"`tracked_repositories` section missing in {config_file}"
+    assert config.branch_mappings, f"`branch_mappings` section missing in {config_file}"
+    assert config.tag_mappings, f"`tag_mappings` section missing in {config_file}"
