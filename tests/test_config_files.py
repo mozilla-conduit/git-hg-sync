3a1bcc06e2f7d176cc47e363987b990b2bbd9ffe
Olivier Mehani <omehani@mozilla.com>
2026-03-27 19:13:37 +1100

    lint: format test_config_files with ruff

--- tests/test_config_files.py
+++ tests/test_config_files.py
@@ -8,10 +8,7 @@ from git_hg_sync.config import Config
 BASEDIR = Path(__file__).parent.parent
 
 
-@pytest.mark.parametrize(
-    "config_file",
-    list(BASEDIR.glob("config-*.toml"))
-)
+@pytest.mark.parametrize("config_file", list(BASEDIR.glob("config-*.toml")))
 def test_config_files(config_file: Path) -> None:
     try:
         config = Config.from_file(config_file)
@@ -21,6 +18,8 @@ def test_config_files(config_file: Path) -> None:
     # We just do a shallow verification. What we really care is that the file could be
     # loaded correctly.
     assert config.pulse, f"`pulse` section missing in {config_file}"
-    assert config.tracked_repositories, f"`tracked_repositories` section missing in {config_file}"
+    assert config.tracked_repositories, (
+        f"`tracked_repositories` section missing in {config_file}"
+    )
     assert config.branch_mappings, f"`branch_mappings` section missing in {config_file}"
     assert config.tag_mappings, f"`tag_mappings` section missing in {config_file}"
