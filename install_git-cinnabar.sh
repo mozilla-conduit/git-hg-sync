5dc5a67fe8468d8c31519b145b10f6eb35f2a079
Olivier Mehani <omehani@mozilla.com>
2026-06-10 13:48:25 +1000

    deps: support passing cinnabar tag to download script
    This makes it download the release tarball from GitHub, rather than
    relying on a transient build from TaskCluster.

--- install_git-cinnabar.sh
+++ install_git-cinnabar.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
-VER=f434ffd01405f62151f702fcf0593fe4c0dd45ea # 0.7.4
+VER=f434ffd01405f62151f702fcf0593fe4c0dd45ea
+# Comment this out if not currently relying on a release.
+TAG=0.7.4
 
 curl https://raw.githubusercontent.com/glandium/git-cinnabar/${VER}/download.py -o download.py
 chmod u+x download.py
-./download.py --exact ${VER}
+./download.py --exact ${TAG:-${VER}}
 chmod a+x git-cinnabar
 chmod a+x git-remote-hg
